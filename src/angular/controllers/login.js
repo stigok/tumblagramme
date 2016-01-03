18a56a960b064bea614a6a1fb6818bb60ff56f67
stigok <stig@stigok.com>
2016-01-03 22:47:47 +0100

    Login improvements

--- src/angular/controllers/login.js
+++ src/angular/controllers/login.js
@@ -1,26 +1,24 @@
 (function () {
   angular.module('tg.Controllers')
-    .controller('LoginCtrl', ['$rootScope', '$scope', '$uibModal',
-      function ($rootScope, $scope, $uibModal) {
-        $scope.openModal = function () {
-          var modal = $uibModal.open({
-            templateUrl: '/templates/login.html',
-            controller: 'LoginModalCtrl'
-          });
+    .controller('LoginCtrl', LoginCtrl);
 
-          modal.result.then(function (username, password) {
-            console.log('on modal closed received', username, password);
-          }, function () {
-            // do nothing on login dismissed
-            console.log('modal dismissed with args; ', arguments);
-          });
-        };
+  function LoginCtrl($rootScope, $scope, $uibModal) {
+    $scope.openModal = function () {
+      var modal = $uibModal.open({
+        templateUrl: '/templates/login.html',
+        controller: 'LoginModalCtrl'
+      });
 
-        $rootScope.login = $scope.openModal;
+      modal.result.then(function (username, password) {
+        console.log('on modal closed received', username, password);
+      }, function () {
+        // do nothing on login dismissed
+        console.log('modal dismissed with args; ', arguments);
+      });
+    };
 
-        $rootScope.$on('event:auth-triggerModal', function () {
-          $scope.openModal();
-        });
-      }
-    ]);
+    $rootScope.$on('event:auth-triggerModal', function () {
+      $scope.openModal();
+    });
+  }
 })();
