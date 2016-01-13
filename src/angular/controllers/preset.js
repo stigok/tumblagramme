a67d94b8ae8e749639bb91cdd9a05f2d47ec1515
stigok <stig@stigok.com>
2016-01-13 04:13:40 +0100

    Preset selection

--- src/angular/controllers/preset.js
+++ src/angular/controllers/preset.js
@@ -4,16 +4,20 @@
 
   function PresetCtrl($scope, $rootScope, $routeParams, Preset, PresetEvents, $log) {
     $scope.presets = Preset.query();
+
+    // Update collection on item update
     $scope.$on(PresetEvents.updated, function () {
       $scope.presets = Preset.query();
     });
 
+    // Determine if it's a new or existing item
     if ($routeParams.id) {
       $scope.preset = Preset.get({id: $routeParams.id});
     } else {
       $scope.preset = new Preset();
     }
 
+    // Schema validation exceptions
     function mongooseErrorCallback(err) {
       if (err.status === 406) {
         $log.log('mongooseErrorCallback', err.data);
