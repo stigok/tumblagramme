0fba07c597d8e8dfb451539547af076bd694b0d9
stigok <stig@stigok.com>
2016-01-14 12:59:24 +0100

    List tags from active preset

--- src/angular/controllers/navigation.js
+++ src/angular/controllers/navigation.js
@@ -2,9 +2,12 @@
   angular.module('tg.Controllers')
     .controller('NavigationCtrl', NavigationCtrl);
 
-  function NavigationCtrl($scope, Tumblr, SessionUser) {
-    $scope.user = SessionUser.get();
+  function NavigationCtrl($scope, Tumblr, SessionUser, Preset) {
+    $scope.user = SessionUser.get(function (user) {
+      $scope.activePreset = Preset.get({id: user.activePresetId});
+    });
     $scope.blogs = Tumblr.blogs();
+
     $scope.setActiveBlog = function (name) {
       $scope.user.tumblr.activeBlogName = name;
       $scope.user.$save();
