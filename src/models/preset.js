876f9e059334828da158b38256609fc9446b82d0
stigok <stig@stigok.com>
2016-01-18 17:24:00 +0100

    Model property validation

--- src/models/preset.js
+++ src/models/preset.js
@@ -1,21 +1,91 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const util = require('util');
 
-var Preset = new Schema({
+const regexps = {
+  tumblrTag: /^[a-zA-Z0-9\-_?!&' ]{3,}$/i,
+  instagramTag: /^[a-zA-Z0-9-_]{3,}$/i,
+  postType: /^photo$/,
+  captionFormat: /^html|markdown$/,
+  state: /^published|draft|queue|private$/
+};
+
+let Preset = new Schema({
   _userId: Schema.Types.ObjectId,
   name: {type: String, required: true},
   blog: {
     name: {type: String, required: true}
   },
   post: {
-    type: {type: String, required: true},
-    caption: String,
-    format: {type: String, default: 'markdown'},
-    state: {type: String, required: true},
-    tags: {type: [String], default: []}
+    type: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return regexps.postType.test(value);
+        },
+        message: 'Invalid post type'
+      }
+    },
+    caption: {type: String, default: ''},
+    format: {
+      type: String,
+      default: 'markdown',
+      validate: {
+        validator: function (value) {
+          return regexps.captionFormat.test(value);
+        },
+        message: 'Invalid post format'
+      }
+    },
+    state: {
+      type: String,
+      default: 'queue',
+      validate: {
+        validator: function (value) {
+          return regexps.state.test(value);
+        },
+        message: 'Invalid post format'
+      }
+    },
+    tags: {
+      type: [String],
+      default: [],
+      validate: {
+        validator: function (value) {
+          if (!util.isArray(value)) {
+            return false;
+          }
+          for (let i = 0; i < value.length; i++) {
+            if (!regexps.tumblrTag.test(value[i])) {
+              return false;
+            }
+          }
+          return true;
+        },
+        message: 'Invalid tag'
+      }
+    }
   },
   instagram: {
-    tags: {type: [String], default: []},
+    tags: {
+      type: [String],
+      default: [],
+      validate: {
+        validator: function (value) {
+          if (!util.isArray(value)) {
+            return false;
+          }
+          for (let i = 0; i < value.length; i++) {
+            if (!regexps.instagramTag.test(value[i])) {
+              return false;
+            }
+          }
+          return true;
+        },
+        message: 'Invalid tag'
+      }
+    },
     autoLike: Boolean,
     autoFollow: Boolean
   }
