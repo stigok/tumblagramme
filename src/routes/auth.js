873b4e043b9e31c295bf43d1cf72be81f077f1bc
stigok <stig@stigok.com>
2016-01-23 19:00:23 +0100

    Tumblr auth

---
+++ src/routes/auth.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = new express.Router();
+
+router.get('/login', function (req, res) {
+  if (req.user) {
+    return res.redirect('/account');
+  }
+  return res.render('views/account/login');
+});
+
+router.use('/api/tumblr', require('./oauth/tumblr'));
+
+router.get('/*', function (req, res, next) {
+  if (!req.user) {
+    return res.redirect('/login');
+  }
+  return next();
+});
+
+router.use('/api/instagram', require('./oauth/instagram'));
+
+router.get('/account', function (req, res) {
+  return res.render('views/account/account');
+});
+
+router.use(function (req, res, next) {
+  if (!req.user) {
+    return res.redirect('/login');
+  }
+  if (!req.user.tumblr || !req.user.instagram) {
+    return res.redirect('/account');
+  }
+  return next();
+});
+
+module.exports = router;
--- src/routes/oauth/tumblr.js
+++ src/routes/oauth/tumblr.js
@@ -1,87 +1,15 @@
 const express = require('express');
 const router = new express.Router();
-const OAuth = require('oauth').OAuth;
+const passport = require('passport');
+// const settings = require('../../../settings.json');
 
-const settings = require('../../../settings.json');
+router.get('/auth', passport.authenticate('tumblr'));
 
-const appConsumerKey = settings.tumblr.consumerKey;
-const appConsumerSecret = settings.tumblr.consumerSecret;
-const callbackUrl = settings.tumblr.callbackUrl;
-
-// Tumblr endpoints
-const authorizeUrl = 'https://www.tumblr.com/oauth/authorize';
-const requestTokenUrl = 'https://www.tumblr.com/oauth/request_token';
-const accessTokenUrl = 'https://www.tumblr.com/oauth/access_token';
-
-// OAuth(requestTokenUrl, accessTokenUrl, consumerKey, consumerSecret, OAuthVersion, callbackUrl, digest)
-const oa = new OAuth(
-  requestTokenUrl,
-  accessTokenUrl,
-  appConsumerKey,
-  appConsumerSecret,
-  '1.0A',
-  callbackUrl,
-  'HMAC-SHA1'
-);
-
-router.get('/auth', function (req, res, next) {
-  console.log('getOAuthRequestToken');
-  oa.getOAuthRequestToken(function (err, token, secret) {
-    if (err) {
-      console.error('\tFailed with error', err);
-      return next(err);
-    }
-    console.log('\ttoken %s | secret %s', token, secret);
-
-    // Save generated tokens to session
-    req.session.requestToken = token;
-    req.session.requestTokenSecret = secret;
-
-    let authUrl = authorizeUrl + '?oauth_token=' + token;
-
-    console.log('Direct client to authUrl');
-    console.log('\t' + authUrl);
-    console.log('\t... waiting for callback');
-
-    return res.redirect(authUrl);
-  });
-});
-
-router.get('/callback', function (req, res, next) {
-  console.log('Received callback');
-  console.log('\toauth_token %s | oauth_verifier %s', req.query.oauth_token, req.query.oauth_verifier);
-  console.log('\tsession token %s | session secret %s', req.session.requestToken, req.session.requestTokenSecret);
-
-  if (!req.session.requestToken || !req.session.requestTokenSecret) {
-    console.error('\tError: Missing session information');
-    return next('No previous session info found');
+router.get('/callback',
+  passport.authenticate('tumblr', {failureRedirect: '/login?from=tumblr_auth_failure'}),
+  function (req, res) {
+    return res.redirect('/account?from=tumblr_auth');
   }
-
-  console.log('getOAuthAccessToken');
-
-  oa.getOAuthAccessToken(
-    req.query.oauth_token,
-    req.session.requestTokenSecret,
-    req.query.oauth_verifier,
-    function (err, token, secret, other) {
-      if (err) {
-        console.error('\tValidation failed with error', err);
-        return next('getOAuthAccessToken failed');
-      }
-      console.log('\ttoken %s | secret %s', token, secret);
-      console.log('other', other);
-
-      req.user.tumblr.token = token;
-      req.user.tumblr.secret = secret;
-
-      req.user.save(function (err) {
-        if (err) {
-          return next('Error saving tumblr token', err);
-        }
-        return res.redirect('/account');
-      });
-    }
-  );
-});
+);
 
 module.exports = router;
